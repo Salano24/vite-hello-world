2ac9f72dbcfd48a3d01f8cef5e321df4c16f599e
Salano24 <salernomarco48@email.com>
2022-11-09 14:20:08 +0100

    fine esercizio

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import HelloWorld from './components/AppTitle.vue'
+
+</script>
+
+<template>
+ 
+ <HelloWorld />
+  
+</template>
+
+<style scoped>
+  
+  h1{
+    color: red;
+   }
+
+</style>
